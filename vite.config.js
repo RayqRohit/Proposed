3a7a48ba2a1005cf7d175b507d113fc6caa991d8
RayqRohit <rrajput1172@gmail.com>
2024-07-15 02:13:27 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig, loadEnv } from 'vite';
 import { createHtmlPlugin } from 'vite-plugin-html';
 
+
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
     const env = loadEnv(mode, process.cwd());
