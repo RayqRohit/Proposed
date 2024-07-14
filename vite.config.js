fecc32316cf1bb07d7ba50c3ba8257c22020e86a
RayqRohit <rrajput1172@gmail.com>
2024-07-15 03:07:59 +0530

    First Commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import react from '@vitejs/plugin-react';
 import { defineConfig, loadEnv } from 'vite';
+
+
 import { createHtmlPlugin } from 'vite-plugin-html';
 
 
@@ -15,6 +17,8 @@ export default defineConfig(({ command, mode }) => {
             createHtmlPlugin({
                 minify: true,
                 inject: { data: { id: env.VITE_APP_GOOGLE_VERIFICATION } },
+                order: 'before',
+                handler: (html) => html,
             }),
         ],
     };
